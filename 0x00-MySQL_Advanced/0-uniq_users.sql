8e500e77b0f4f2f1391b89aa14ee2b5dda3214d8
mohamed hosam <mohamedhosamopp122@icloud.com>
2024-08-29 06:25:25 +0000

    hi mom

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,7 +5,7 @@
 --If the table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
